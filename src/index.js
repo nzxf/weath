13716d65902202dca9472dab59c00c4342594efa
nzxf <122195155+nzxf@users.noreply.github.com>
2023-06-11 13:55:27 +0700

    Add async function for weather and forecast

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 
 const API_KEY_WEATHER = 'e1d35972d5eb49b5b3b154449231006';
-const API_KEY_GIPHY = '0aDlSjG2WKUWJKfdTJDwj2vLjFjhrn9z';
 
 const elementMaker = (type, parent, text, ...clasNames) => {
   const element = document.createElement(type);
@@ -16,15 +15,27 @@ const elementMaker = (type, parent, text, ...clasNames) => {
 };
 
 const content = document.querySelector('#content');
-const img = elementMaker('img', content, '', 'img');
 
-const callImage = async (apiKey,keyword) => {
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${keyword}`
+const tellWeather = async (api, city) => {
+  const url = 'http://api.weatherapi.com/v1/';
+  const res = await fetch(`${url}current.json?key=${api}&q=${city}`);
+  const data = await res.json();
+  console.log(data.current.condition.text);
+};
+const tellForecast = async (api, zipcode, days) => {
+  const url = 'http://api.weatherapi.com/v1/';
+  const res = await fetch(
+    `${url}forecast.json?key=${api}&q=${zipcode}&days=${days}`
   );
-  const imageData = await response.json()
-//   console.log(imageData)
-  img.src = imageData.data.images.original.webp
+  const data = await res.json();
+};
+const searchCity = async (api, input) => {
+  const url = 'http://api.weatherapi.com/v1/';
+  const res = await fetch(`${url}search.json?key=${api}&q=${input}`);
+  const data = await res.json();
+  console.log(data);
 };
 
-callImage(API_KEY_GIPHY, 'cats');
+// tellWeather(API_KEY_WEATHER, 'depok');
+
+// searchCity(API_KEY_WEATHER, 'jaka')
