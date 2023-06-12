6035bed99f29b54636c08877d592c20a7ffe84ce
nzxf <122195155+nzxf@users.noreply.github.com>
2023-06-12 13:58:51 +0700

    Refactor element maker and appender

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import './style.css';
 
 const API_KEY_WEATHER = 'e1d35972d5eb49b5b3b154449231006';
 
-const elementMaker = (type, parent, text, ...clasNames) => {
-  const element = document.createElement(type);
-  if (clasNames) {
-    for (let i = 0; i < clasNames.length; i++) {
-      element.classList.add(clasNames[i]);
-    }
+const elementMaker = (type, parent, text = '', ...classNames) => {
+  if (!type || typeof type !== 'string') {
+    throw new Error(`Invalid value for 'type' parameter`);
   }
-  element.textContent = text;
+  if (!(parent instanceof Element)) {
+    throw new Error(`'parent' parameter must be valid DOM element`);
+  }
+  const element = document.createElement(type);
+  if (classNames.length > 0) {
+    classNames.forEach((className) => element.classList.add(className));
+  }
+  if (typeof text === 'string' && text !== '') {
+    element.textContent = text;
+  }
   parent.appendChild(element);
   return element;
 };
-
-const content = document.querySelector('#content');
 
 const tellWeather = async (api, city) => {
   const url = 'http://api.weatherapi.com/v1/';
   const res = await fetch(`${url}current.json?key=${api}&q=${city}`);
   const data = await res.json();
-  console.log(data.current.condition.text);
+  // console.log(data.current.condition.text);
+  return data;
 };
 const tellForecast = async (api, zipcode, days) => {
   const url = 'http://api.weatherapi.com/v1/';
@@ -35,7 +40,19 @@ const searchCity = async (api, input) => {
   const data = await res.json();
   console.log(data);
 };
-
+tellWeather(API_KEY_WEATHER, 'london');
 // tellWeather(API_KEY_WEATHER, 'depok');
-
 // searchCity(API_KEY_WEATHER, 'jaka')
+
+const content = document.querySelector('#content');
+const main = document.querySelector('.main');
+const cities = document.querySelectorAll('.city');
+cities.forEach(async (city) => {
+  const data = await tellWeather(API_KEY_WEATHER, 'amsterdam');
+  elementMaker('div', city, data.location.name);
+  elementMaker('div', city, data.location.country);
+  elementMaker('div', city, data.current.temp_c);
+  elementMaker('div', city, data.current.condition.text);
+  // elementMaker('div', city, data.current.wind_kph)
+  // elementMaker('div', city, data.current.humidity)
+});
